fa4b62c6bd07aae643b1bb7cab291c0dcc84303f
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-06-26 22:50:56 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar/Navbar";
 import Hero from "@/components/Hero/Hero";
 import Services from "@/components/Services/Services";
 import Works from "@/components/Works/Works";
+import Works from "@/components/Clients/Clients";
 import Stories from "@/components/Stories/Stories";
 import FAQ from "@/components/FAQ/FAQ";
 import Contact from "@/components/Contact/Contact";
@@ -14,6 +15,7 @@ export default function HomePage() {
       <Hero />
       <Services />
       <Works />
+      <Clients />
       <Stories />
       <FAQ />
       <Contact />
