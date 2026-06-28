fecf1168725f6ebb69ffcea5d79a020357cdb02e
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-06-28 12:51:50 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Navbar from "@/components/Navbar/Navbar";
-import Hero from "@/components/Hero/Hero";
-import Services from "@/components/Services/Services";
-import Works from "@/components/Works/Works";
-import Blog from "@/components/Blog/Blog";
-import FAQ from "@/components/FAQ/FAQ";
-import Contact from "@/components/Contact/Contact";
-import Footer from "@/components/Footer/Footer";
+import Navbar from "@/components/sections/Navbar/Navbar";
+import Hero from "@/components/sections/Hero/Hero";
+import Services from "@/components/sections/Services/Services";
+import Works from "@/components/sections/Works/Works";
+import Blog from "@/components/sections/Blog/Blog";
+import FAQ from "@/components/sections/FAQ/FAQ";
+import Contact from "@/components/sections/Contact/Contact";
+import Footer from "@/components/sections/Footer/Footer";
 
 export default function HomePage() {
   return (
