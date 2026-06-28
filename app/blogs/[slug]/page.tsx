cf70417b6a484bad1872ce1b3d460500f8542678
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-06-28 12:35:34 +0600

    Update page.tsx

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -1,6 +1,5 @@
-import type { Metadata } from "next";
-import Image from "next/image";
 import { notFound } from "next/navigation";
+import type { Metadata } from "next";
 
 import {
   getAllBlogs,
@@ -33,45 +32,27 @@ export async function generateMetadata({
 
   if (!blog) {
     return {
-      title: "Blog Not Found | PrimeDigitor",
+      title: "Blog Not Found",
     };
   }
-
-  const site =
-    process.env.NEXT_PUBLIC_SITE_URL ??
-    "https://primedigitor.com";
 
   return {
     title: blog.seoTitle ?? blog.title,
     description:
       blog.seoDescription ?? blog.excerpt,
 
-    alternates: {
-      canonical: `${site}/blogs/${blog.slug}`,
-    },
-
     openGraph: {
       title: blog.title,
       description: blog.excerpt,
-      url: `${site}/blogs/${blog.slug}`,
+      images: [blog.coverImage],
       type: "article",
-
-      images: [
-        {
-          url: `${site}${blog.coverImage}`,
-          width: 1200,
-          height: 630,
-          alt: blog.title,
-        },
-      ],
     },
 
     twitter: {
       card: "summary_large_image",
       title: blog.title,
       description: blog.excerpt,
-
-      images: [`${site}${blog.coverImage}`],
+      images: [blog.coverImage],
     },
   };
 }
@@ -86,10 +67,6 @@ export default async function BlogDetailPage({
   if (!blog) {
     notFound();
   }
-
-  const site =
-    process.env.NEXT_PUBLIC_SITE_URL ??
-    "https://primedigitor.com";
 
   return (
     <main className={styles.page}>
@@ -114,12 +91,9 @@ export default async function BlogDetailPage({
           <span>{blog.readingTime}</span>
         </div>
 
-        <Image
+        <img
           src={blog.coverImage}
           alt={blog.title}
-          width={1200}
-          height={700}
-          priority
           className={styles.cover}
         />
       </section>
@@ -130,46 +104,14 @@ export default async function BlogDetailPage({
             __html: blog.content,
           }}
         />
-                <ShareButtons
+
+        <ShareButtons
           title={blog.title}
-          url={`${site}/blogs/${blog.slug}`}
+          url={`https://primedigitor.com/blogs/${blog.slug}`}
         />
       </article>
 
       <RelatedBlogs currentBlog={blog} />
-
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify({
-            "@context": "https://schema.org",
-            "@type": "BlogPosting",
-
-            headline: blog.title,
-
-            description: blog.excerpt,
-
-            image: `${site}${blog.coverImage}`,
-
-            author: {
-              "@type": "Organization",
-              name: blog.author,
-            },
-
-            publisher: {
-              "@type": "Organization",
-              name: "PrimeDigitor",
-            },
-
-            datePublished: blog.publishedAt,
-
-            mainEntityOfPage: {
-              "@type": "WebPage",
-              "@id": `${site}/blogs/${blog.slug}`,
-            },
-          }),
-        }}
-      />
     </main>
   );
 }
