16b05909b6b57e30c1cb5b5cf5d031509c6d2300
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-07-10 14:46:10 +0600

    Update Contact.tsx

--- components/sections/Contact/Contact.tsx
+++ components/sections/Contact/Contact.tsx
@@ -132,7 +132,7 @@ export default function Contact() {
         <div className={styles.optionGrid}>
 
           <a
-            href="https://wa.me/8801641572608?text=Hello%20PrimeDigitor!%20I'm%20interested%20in%20your%20services.%20I'd%20like%20to%20discuss%20my%20project."
+            href="https://wa.me/8801832729052?text=Hello%20PrimeDigitor!%20I'm%20interested%20in%20your%20services.%20I'd%20like%20to%20discuss%20my%20project."
             target="_blank"
             rel="noopener noreferrer"
             className={styles.optionCard}
