d71ee7989515bd3ee56aff8b70a83ee349f9dd85
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-07-11 02:52:44 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import Script from "next/script";
+import FloatingChat from "@/components/FloatingChat/FloatingChat";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://www.primedigitor.com"),
@@ -93,7 +95,30 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+
+        <FloatingChat />
+
+        <Script
+          id="tawk-to"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),
+                    s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/YOUR_PROPERTY_ID/default';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
+      </body>
     </html>
   );
 }
