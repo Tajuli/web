b0e85cca3cbd99c46083e30db7c8de09c9534030
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-07-11 02:48:17 +0600

    Update FloatingChat.tsx

--- components/FloatingChat/FloatingChat.tsx
+++ components/FloatingChat/FloatingChat.tsx
@@ -1,28 +1,34 @@
 "use client";
 
-import Script from "next/script";
+import { MessageCircle } from "lucide-react";
+import styles from "./FloatingChat.module.css";
+
+declare global {
+  interface Window {
+    Tawk_API?: any;
+  }
+}
 
 export default function FloatingChat() {
+  const openChat = () => {
+    if (window.Tawk_API) {
+      window.Tawk_API.maximize();
+    }
+  };
+
   return (
-    <>
-      <Script
-        id="tawk"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-            (function(){
-              var s1=document.createElement("script"),
-              s0=document.getElementsByTagName("script")[0];
-              s1.async=true;
-              s1.src='https://embed.tawk.to/YOUR_PROPERTY_ID/default';
-              s1.charset='UTF-8';
-              s1.setAttribute('crossorigin','*');
-              s0.parentNode.insertBefore(s1,s0);
-            })();
-          `,
-        }}
-      />
-    </>
+    <button
+      className={styles.chatButton}
+      onClick={openChat}
+      aria-label="Live Chat"
+    >
+      <span className={styles.online}></span>
+
+      <MessageCircle size={28} strokeWidth={2.2} />
+
+      <span className={styles.tooltip}>
+        Chat with us
+      </span>
+    </button>
   );
 }
