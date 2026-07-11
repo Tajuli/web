55d1829bcdf8b6ba8f00199f30adf5c7405d3196
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-07-11 15:13:18 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -98,26 +98,18 @@ export default function RootLayout({
       <body>
         {children}
 
-        <FloatingChat />
-
         <Script id="tawk-to" strategy="afterInteractive">
-{`
-  var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-
-Tawk_API = {
-  startHidden: true
-};
-
-  (function(){
-    var s1=document.createElement("script"),
-        s0=document.getElementsByTagName("script")[0];
-    s1.async=true;
-    s1.src='https://embed.tawk.to/6a51572b8926201d475f55a9/default';
-    s1.charset='UTF-8';
-    s1.setAttribute('crossorigin','*');
-    s0.parentNode.insertBefore(s1,s0);
-  })();
-`}
+  {`
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+      var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+      s1.async=true;
+      s1.src='https://embed.tawk.to/6a51572b8926201d475f55a9/default';
+      s1.charset='UTF-8';
+      s1.setAttribute('crossorigin','*');
+      s0.parentNode.insertBefore(s1,s0);
+    })();
+  `}
 </Script>
       </body>
     </html>
