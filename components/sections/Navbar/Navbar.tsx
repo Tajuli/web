f8729f7c5245c2c65c1b53e336ff50fbb8bdba89
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-07-13 22:31:44 +0600

    Update Navbar.tsx

--- components/sections/Navbar/Navbar.tsx
+++ components/sections/Navbar/Navbar.tsx
@@ -9,7 +9,7 @@ const navItems = [
   { label: "Home", href: "#hero", id: "hero" },
   { label: "Services", href: "#services", id: "services" },
   { label: "Works", href: "#work", id: "work" },
-  { label: "About", href: "#whychooseus", id: "whychooseus" },
+  { label: "Why-Choose-Us", href: "#whychooseus", id: "whychooseus" },
   { label: "Blog", href: "#blog", id: "blog" },
   { label: "FAQ", href: "#faq", id: "faq" },
   { label: "Contact", href: "#contact", id: "contact" },
