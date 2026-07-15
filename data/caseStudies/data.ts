9c15f93186a075c1b177e72b2fcdc303336e4e9b
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-07-15 11:52:40 +0600

    Create data.ts

---
+++ data/caseStudies/data.ts
@@ -0,0 +1,11 @@
+import { CaseStudy } from "./type";
+
+import { proAid } from "./caseStudy/proaid";
+import { drTelottomaRahman } from "./caseStudy/dr-telottoma-rahman";
+import { primeDigitor } from "./caseStudy/primedigitor";
+
+export const caseStudies: CaseStudy[] = [
+  proAid,
+  drTelottomaRahman,
+  primeDigitor,
+];
