aa93cb9a5e882dc781efdf0b5837b8f50f347128
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-07-17 03:18:02 +0600

    Update data.ts

--- data/caseStudies/data.ts
+++ data/caseStudies/data.ts
@@ -3,7 +3,7 @@ import { CaseStudy } from "./type";
 import { proAid } from "./caseStudy/proaid";
 import { drTelottomaRahman } from "./caseStudy/dr-telottoma-rahman";
 import { primeDigitor } from "./caseStudy/primedigitor";
-import { primeDigitor } from "./caseStudy/x-real-estate";
+import { xRealEstate } from "./caseStudy/x-real-estate";
 
 export const caseStudies: CaseStudy[] = [
   proAid,
