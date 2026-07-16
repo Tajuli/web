79c5c05eef19dacad1416efcbaea1de9191e1d0a
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-07-17 03:36:27 +0600

    Update index.ts

--- data/caseStudies/index.ts
+++ data/caseStudies/index.ts
@@ -4,3 +4,5 @@ export * from "./data";
 export * from "./caseStudy/proaid";
 export * from "./caseStudy/dr-telottoma-rahman";
 export * from "./caseStudy/primedigitor";
+export * from "./caseStudy/x-real-estate";
+export * from "./caseStudy/unseenbdeshi-facebook-marketing";
