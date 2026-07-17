bb499f94d728e4feae8992c9064077a6f02905f9
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-07-17 23:10:11 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Navbar from "@/components/sections/Navbar/Navbar";
 import Hero from "@/components/sections/Hero/Hero";
 import Services from "@/components/sections/Services/Services";
 import Works from "@/components/sections/Works/Works";
@@ -7,12 +6,10 @@ import WhyChooseUs from "@/components/sections/WhyChooseUs/WhyChooseUs";
 import Blog from "@/components/sections/Blog/Blog";
 import FAQ from "@/components/sections/FAQ/FAQ";
 import Contact from "@/components/sections/Contact/Contact";
-import Footer from "@/components/sections/Footer/Footer";
 
 export default function HomePage() {
   return (
     <main className="bg-page text-white">
-      <Navbar />
       <Hero />
       <Services />
       <Works />
@@ -21,7 +18,6 @@ export default function HomePage() {
       <Blog />
       <FAQ />
       <Contact />
-      <Footer />
     </main>
   );
 }
